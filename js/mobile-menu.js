6d4840dd03241e57d22ca5149c16bd00eadafc28
GGalina <71042425+GGalina@users.noreply.github.com>
2023-01-25 17:57:49 +0400

    HW with changes

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -4,7 +4,7 @@
     closeMenuBtn: document.querySelector('[data-menu-close]'),
     menu: document.querySelector('[data-menu]'),
     body: document.querySelector('body'),
-    menuList: document.querySelector('.mob-menu__list'),
+    menuList: document.querySelector('.mob-menu-list'),
   };
 
   refs.openMenuBtn.addEventListener('click', toggleMenu);
